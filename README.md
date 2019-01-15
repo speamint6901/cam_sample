bec77830fd123e9238f7c0a0c5c298fcb2597fb1
ju_nasu <ju_nasu@fs96f9c21d.tkyc008.ap.nuro.jp>
2019-01-15 18:32:12 +0900

    README更新

--- README.md
+++ README.md
@@ -11,3 +11,24 @@
 git でcloneする
 https://github.com/speamint6901/cam_sample.git
 
+##### 新規イメージ作成（時間がかかります。基本は初回のみで問題ありません。）  
+cloneしたディレクトリで下記を実行
+```
+$ docker-compose build
+```
+
+##### コンテナ立ち上げ  
+```
+$ docker-compose up -d
+```
+
+##### 初期構築
+```
+$ sh containers/app/init.sh
+```
+
+##### 管理ツール用のドメイン追加(ローカル)
+hostsファイルに以下を追記
+```
+127.0.0.1	localhost.admin
+```
--- containers/app/init.sh
+++ containers/app/init.sh
@@ -3,6 +3,7 @@
 docker-compose exec -w /var/www/html/campbug app /usr/local/bin/composer.phar install
 docker-compose exec -w /var/www/html/campbug app cp .env.example .env
 docker-compose exec -w /var/www/html/campbug app php artisan key:generate
+docker-compose exec -w /var/www/html/campbug app php artisan migrate
 docker-compose exec -w /var/www/html/campbug app npm install
 docker-compose exec -w /var/www/html/campbug app npm install vuex --save
 docker-compose exec -w /var/www/html/campbug app npm install vue-router
