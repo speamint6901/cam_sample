4a26eb05e41b20a6cdd727b6bd87a6e6cbea4c10
ju_nasu <ju_nasu@fs96f9c21d.tkyc008.ap.nuro.jp>
2019-02-08 13:38:57 +0900

    sample compose

--- containers/app/init.sh
+++ containers/app/init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker-compose exec -w /var/www/html/campbug app /usr/local/bin/composer.phar install
-docker-compose exec -w /var/www/html/campbug app cp .env.example .env
-docker-compose exec -w /var/www/html/campbug app php artisan key:generate
-docker-compose exec -w /var/www/html/campbug app php artisan migrate
-docker-compose exec -w /var/www/html/campbug app npm install
-docker-compose exec -w /var/www/html/campbug app npm install vuex --save
-docker-compose exec -w /var/www/html/campbug app npm install vue-router
-docker-compose exec -w /var/www/html/campbug app npm run dev
+docker-compose exec app /usr/local/bin/composer.phar install
+docker-compose exec app cp .env.example .env
+docker-compose exec app php artisan key:generate
+docker-compose exec app php artisan migrate
+docker-compose exec app npm install
+docker-compose exec app npm install vuex --save
+docker-compose exec app npm install vue-router
+docker-compose exec app npm run dev
