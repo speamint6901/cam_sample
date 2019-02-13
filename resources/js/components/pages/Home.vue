ac45c3c589d3b8bd8d602e2692fec45c20cedaae
2Q <fresh_2q@yahoo.co.jp>
2019-02-13 23:03:29 +0900

    HomeにひとまずArticleCard作成

--- resources/js/components/pages/Home.vue
+++ resources/js/components/pages/Home.vue
@@ -1,5 +1,55 @@
 <template>
- <div class="l-Contents">
-  HOME
+ <div class="l-Contents u-SiteSize">
+
+  <div class="l-Contents_Block">
+   <div class="l-Feed">
+
+
+    <article class="c-ArticleCard">
+
+     <header class="c-ArticleCard_Header">
+      <a href="">
+       <img class="c-ArticleCard_Heading-Img u-ObjectFitImg" src="https://www.goldwin.co.jp/ec/img/l/NP71877.jpg" alt="GearName" />
+      </a>
+     </header>
+
+
+     <div class="c-ArticleCard_Overview">
+      <a class="c-ArticleCard_SubTtile u-Hedding_Card_Sub">
+       BrandName
+      </a><!--//.c-ArticleCard_SubTtile-->
+      <h1 class="c-ArticleCard_Title u-Hedding_Card">
+       <a href="" class="u-TxtColor_Txt">
+        Gear Name
+       </a>
+      </h1><!--//.c-ArticleCard_Title-->
+     </div><!--//_Overview-->
+
+
+     <footer class="c-ArticleCard_Footer">
+      <div class="c-ArticleCard_Status">
+       <ul class="c-ArticleCard_Status_Wrap">
+        <li class="c-ArticleCard_Status_Item">
+         <i class="c-ArticleCard_Status-Icon">H.</i>
+         <span class="c-ArticleCard_Status-Count">888T</span>
+        </li>
+        <li class="c-ArticleCard_Status_Item">
+         <i class="c-ArticleCard_Status-Icon">W.</i>
+         <span class="c-ArticleCard_Status-Count">888T</span>
+        </li>
+        <li class="c-ArticleCard_Status_Item">
+         <i class="c-ArticleCard_Status-Icon">C.</i>
+         <span class="c-ArticleCard_Status-Count">888T</span>
+        </li>
+       </ul>
+      </div><!-- /.c-ArticleCard_Status -->
+     </footer>
+
+    </article>
+
+
+   </div><!--//.l-Feed-->
+  </div><!-- /.l-Contents_Block -->
+
  </div><!--//l-Contents-->
 </template>
