625f3d6864b58f0d2fcb326e74939641c37414f5
2Q <fresh_2q@yahoo.co.jp>
2019-02-15 20:07:37 +0900

    /home  ・ArticleCard修正とUserPhoto用のArticleCard追加

--- resources/js/components/pages/Home.vue
+++ resources/js/components/pages/Home.vue
@@ -4,7 +4,7 @@
   <div class="l-Contents_Block">
    <div class="l-Feed">
 
-
+    <!--通常のArticleCard-->
     <article class="c-ArticleCard">
 
      <header class="c-ArticleCard_Header">
@@ -43,10 +43,80 @@
         </li>
        </ul>
       </div><!-- /.c-ArticleCard_Status -->
+
+      <div class="c-ArticleCard_Rating">
+       <div class="c-ArticleCard_Rating-Thunder">
+        <img class="" src="/img/Dummt_Thunder.svg" alt="UserName" />
+       </div><!-- /.c-ArticleCard_Rating-Thunder -->
+       <p class="c-ArticleCard_Rating-Score u-Text_Main-S">
+        3.4 THUNDER
+       </p><!--//.c-ArticleCard_Rating-Score-->
+      </div>
      </footer>
 
     </article>
 
+
+    <!--UserPhoto-->
+    <article class="c-ArticleCard">
+
+     <header class="c-ArticleCard_Header">
+      <a href="">
+       <img class="c-ArticleCard_Heading-Img u-ObjectFitImg" src="https://camp-mania.com/wp/wp-content/uploads/cm-fjs-1-720x540.jpg" alt="GearName" />
+      </a>
+
+      <div class="c-ArticleCard_Header_Info u-Flex _AlignItem-Center">
+       <div class="c-UserIcon u-Flex _AlignItem-Center">
+        <div class="c-UserIcon_Thumb">
+         <a href="">
+          <img class="c-UserIcon_Thumb-CardImg u-ObjectFitImg" src="/img/user01.png" alt="UserName" />
+         </a>
+        </div><!--//.c-UserIcon_Thumb-->
+        <p class="c-UserIcon_Name u-Text_Main-XS u-TxtColor_White">UserName</p><!--//.c-UserIcon_Name-->
+       </div><!--//.c-UserIcon-->
+       <button type="button" name="like_btn" value="" class="c-ArticleCard_Btn -Like">
+        LIKE
+       </button>
+      </div>
+     </header>
+
+
+     <div class="c-ArticleCard_Overview">
+      <a class="c-ArticleCard_SubTtile u-Hedding_Card_Sub">
+       BrandName
+      </a><!--//.c-ArticleCard_SubTtile-->
+      <h1 class="c-ArticleCard_Title u-Hedding_Card">
+       <a href="" class="u-TxtColor_Txt">
+        Gear Name
+       </a>
+      </h1><!--//.c-ArticleCard_Title-->
+     </div><!--//_Overview-->
+
+
+     <footer class="c-ArticleCard_Footer">
+      <div class="c-ArticleCard_Status">
+       <ul class="c-ArticleCard_Status_Wrap">
+        <li class="c-ArticleCard_Status_Item -Active">
+         <i class="c-ArticleCard_Status-Icon">H.</i>
+         <span class="c-ArticleCard_Status-Count">888T</span>
+        </li>
+        <li class="c-ArticleCard_Status_Item">
+         <i class="c-ArticleCard_Status-Icon">W.</i>
+         <span class="c-ArticleCard_Status-Count">888T</span>
+        </li>
+        <li class="c-ArticleCard_Status_Item">
+         <i class="c-ArticleCard_Status-Icon">C.</i>
+         <span class="c-ArticleCard_Status-Count">888T</span>
+        </li>
+        <li class="c-ArticleCard_Status_Item">
+         <i class="c-ArticleCard_Status-Icon">L.</i>
+         <span class="c-ArticleCard_Status-Count">50</span>
+        </li>
+       </ul>
+      </div><!-- /.c-ArticleCard_Status -->
+     </footer>
+
+    </article>
 
    </div><!--//.l-Feed-->
   </div><!-- /.l-Contents_Block -->
