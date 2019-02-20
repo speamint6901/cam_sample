13c1a40a50af60cfac5449abc2fa9320c5f51719
j-nasu <speamint6901@gmail.com>
2019-02-20 21:21:51 +0900

    deploy.sh反映

--- containers/app/deploy.sh
+++ containers/app/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 git pull origin develop
-docker-compose exec app cp .env.example .env
 docker-compose exec app /usr/local/bin/composer.phar install
 docker-compose exec app php artisan migrate
 docker-compose exec app npm install
