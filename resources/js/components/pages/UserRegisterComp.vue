9e0ddb285e10908c65f4cd4b1ce45ebe7dd30f5e
2Q <fresh_2q@yahoo.co.jp>
2019-02-21 05:46:27 +0900

    /register ・Loader修正 /register/complete 文章修正とスタイル

--- resources/js/components/pages/UserRegisterComp.vue
+++ resources/js/components/pages/UserRegisterComp.vue
@@ -1,7 +1,30 @@
 <template>
 
-<div>
-    確認のメールを送信しました
-</div>
+ <div class="l-Contents_Block u-Padding_pdTop-base10 u-Padding_pdBottom-base10">
+
+  <h2 class="c-Aricle_Hedding u-Hedding_Page u-TxAlign-Center">確認メールを送信しました</h2>
+
+  <div class="l-Contents_Block-M">
+
+   <p class="u-Margin _mgBottom-base10">
+    指定のメールアドレスへ確認のメールを送信いたしました。<br>
+    確認メールにあります手順に従って本登録の完了をお願いします。
+   </p>
+
+
+  <div class="p-Note">
+
+   <p class="u-Margin _mgBottom-base02"> 確認メールが届かない場合、下記をご確認ください。</p>
+   <ul class="u-ListStyle-BulletPoint u-Text_Main-S">
+    <li>迷惑メールフォルダにメールが振り分けられている可能性があります。迷惑メールフォルダも含めご確認ください。</li>
+    <li>ドメイン指定受信、シークレットコード指定、メール一括拒否、メール指定受信など、一部のメールが届かなくなる設定にしていると、登録確認メールが届かない場合があります。「camp-bug.com」からのメールを受信できるように設定してください。</li>
+    <li>メールアドレスを間違って入力した可能性があります。再度、登録を行ってください。</li>
+   </ul>
+   </div><!--//.p-Note-->
+
+  </div><!--//.l-Contents_Block-M-->
+
+
+ </div><!--//.l-Contents_Block-->
 
 </template>
