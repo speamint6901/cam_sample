2cd32fad6c79ef6e8a486a131531b3ac3a9f0cd3
j-nasu <speamint6901@gmail.com>
2019-03-06 22:20:54 +0900

    開閉メニュー

--- resources/js/components/object/component/Header.vue
+++ resources/js/components/object/component/Header.vue
@@ -60,19 +60,21 @@
       <img class="c-UserIcon_Thumb-Img u-ObjectFitImg" src="/img/user01.png" alt="UserName" />
      </div><!--//.c-UserIcon_Thumb-->
     </div><!--//.c-UserIcon-->
-    <div id="dd-menu">
-      <ul class="list">
-        <li class="list-item">
-          <a href="#" @click="dropdown()">menu</a>
-          <ul v-if="active">
-            <li v-for="item in sublistlabel" class="sublist-item">
-              <a href="#" v-bind:label="item">{{item}}</a>
-            </li>
-          </ul>
-        </li>
-      </ul>
+    <div v-cloak>
+      <div id="header">
+        <menu-btn 
+           :class="{ menu_btn: true, open_: menuFlag }" 
+	   @click.native="menuToggle">
+	</menu-btn>
+      </div>
+      <transition>
+        <div id="sp_menu" v-show="menuFlag" :style="{ marginTop: headH }">
+          <div class="menu_wrapp">
+	    Menu
+	  </div>
+        </div>
+      </transition>
     </div>
-
    </div><!-- /.c-Header_Block -->
 
   </div><!-- /.c-Header -->
@@ -83,10 +85,22 @@
 <script>
  
 import { mapState } from 'vuex';
+var menuBtnComp = {
+  template: `
+    <div>
+        <span class="menu_trigger">
+            <span></span><span></span><span></span>
+        </span>
+    </div>
+`
+}
+
 export default {
 
   data() {
-  	return {
+    return {
+        menuFlag: false,
+        headH: "0px",
         sublistlabel:{
             "Cloth":"",
             "Pants":"",
@@ -102,56 +116,110 @@ export default {
     }),
   },
   methods: {
-  	dropdown(list){
-        console.log(this.active);
-    	this.active = this.active ? false:true;
+    menuToggle: function() {
+      //menuFlag切り替え
+      this.menuFlag = !this.menuFlag;
     }
+  },
+  mounted: function() {
+    //headerの高さ取得
+    let h = document.getElementById("header").offsetHeight;
+    this.headH = h + "px";
+  },
+  components: {
+    'menu-btn': menuBtnComp,
   }
-
 }
 
 </script>
 
 <style>
-#dd-menu{
-  width:300px;
+[v-cloak]{
+  display: none;
 }
-ul{
-  padding: 0;
+@keyframes show{
+    from{
+        opacity: 0;
+    }
+    to{
+        opacity: 1;
+    }
 }
-li{
-  list-style:none;
+header{
+  padding: 16px;
+  background: #f3f3f3;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 100;
 }
-a{
-  color:#333;
-  text-decoration:none;
+
+.menu_btn{
+    width: 10vw;
+    height: 10vw;
+    max-width: 60px;
+    max-height: 60px;
+    padding: 2% 0;
+    cursor: pointer;
+
+    .menu_trigger{
+        position: relative;
+        display: block;
+        width: 100%;
+        height: 100%;      
+        span{
+            position: absolute;
+            left: 0;
+            display: block;
+            width: 100%;
+            height: 2px;
+            background: #333;
+            border-radius: 2px;
+            transition: all .4s;
+
+            &:nth-of-type(1) {
+                top: 0;
+            }
+            &:nth-of-type(2) {
+                top: calc(50% - 1px);
+            }
+            &:nth-of-type(3) {
+                bottom: 0;
+            }
+        }
+    }
+
+    //メニュー開いたとき
+    &.open_{
+        .menu_trigger span:nth-of-type(1) {
+            top: calc(50% - 1px);
+            transform: rotate(-45deg);
+        }
+        .menu_trigger span:nth-of-type(2) {
+            opacity: 0;
+        }
+        .menu_trigger span:nth-of-type(3) {
+            bottom: calc(50% - 1px);
+            transform: rotate(45deg);
+        }
+    }
 }
-.list{
-  padding: 0;
-  border:solid 1px #ccc;
-}
-.list-item a{
-  position: relative;
-  padding: 8px;
-  border-bottom:solid 1px #ccc;
-}
-.list-item a{
-  display: block;
-}
-.list-item:last-of-type a{
-  border-bottom:none;
-}
-.list-item > a::after{
-  content:"▼";
-  position: absolute;
-  top:12px;
-  right:10px;
-  font-size:10px;
-}
-.list-item >a.active::after{
-  content:"▲";
-}
-.sublist-item{
-  font-size:0.75rem;
+
+
+#sp_menu{
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 99;
+  padding: 5%;
+  background: rgba(0,0,0,0.9);
+  transition: all 0.5s;
+  color: #fff;
+  &.v-enter, &.v-leave-to {
+        opacity: 0;
+    }
 }
 </style>
