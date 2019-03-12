03fe2fe1d885f4683676700f4fec333644074c1d
ju_nasu <ju_nasu@fs96f9c21d.tkyc008.ap.nuro.jp>
2019-03-12 12:23:37 +0900

    exportサンプル

--- containers/db/export_db.sh
+++ containers/db/export_db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose exec db mysqldump -uroot -p -r /etc/mysql/dump/campbug.sql campbug
+docker-compose exec db mysqldump -uroot -psecret --no-create-info campbug gears gear_profiles categories big_categories genres brands brand_groups -r /etc/mysql/dump/campbug.sql
