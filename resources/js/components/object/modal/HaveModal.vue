2b6dfd12c566484bc2b4d5f7ad00ed312e8fa6c1
2Q <fresh_2q@yahoo.co.jp>
2019-03-13 17:03:32 +0900

    Modal 修正

--- resources/js/components/object/modal/HaveModal.vue
+++ resources/js/components/object/modal/HaveModal.vue
@@ -5,7 +5,11 @@
   <div class="c-Modal-Have">
    <header class="c-Modal-Have_Header">
     <h2 class="u-Text_Main-L u-TxtColor_White">Have : Review</h2>
-   <button class="c-Modal-Have_Close" @click="hideModal">closeModal</button>
+   <button class="c-Modal-Have_Close" @click="hideModal">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="c-Modal-Have_Close-Img">
+     <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+    </svg>
+   </button>
   </header>
 
    <div class="c-Modal-Have_Content">
@@ -17,12 +21,11 @@
     </div><!-- /.c-Modal-Have_RatingSlider -->
 
     <textarea name="" id="Input_Gear_Review" cols="30" rows="25" class="c-Modal-Have_Textarea" placeholder="レビューを記入してください。無記入でも登録できます。"></textarea>
-    <textarea name="" id="Input_Gear_Tag" cols="30" rows="5" class="c-Modal-Have_Textarea" placeholder="#タグ"></textarea>
 
    </div><!-- /.c-Modal-Have_Content -->
 
    <footer class="c-Modal-Have_Footer">
-    <button>登録</button>
+    <button type="submit" class="c-Form_Submit">登録</button>
    </footer>
   </div><!--//c-Modal-Have-->
 
