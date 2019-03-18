c8300ddc6413a0ec2eb0cddf718dabca890d6758
ju_nasu <ju_nasu@fs96f9c21d.tkyc008.ap.nuro.jp>
2019-03-18 17:41:21 +0900

    ボタンサンプル

--- resources/js/components/object/modal/HaveModal.vue
+++ resources/js/components/object/modal/HaveModal.vue
@@ -1,7 +1,6 @@
 <template>
  <div class="c-Modal">
   <div class="c-Modal_Bg"></div>
-  <form @submit.prevent="register" class="l-Contents_Block-XS">
   <div class="c-Modal-Have">
    <header class="c-Modal-Have_Header">
     <h2 class="u-Text_Main-L u-TxtColor_White">Have : Review</h2>
@@ -12,23 +11,24 @@
    </button>
   </header>
 
+  <form @submit.prevent="toggleHave" class="l-Contents_Block-XS">
    <div class="c-Modal-Have_Content">
 
     <div class="c-Modal-Have_Rating">
      <p class="c-Modal-Have_Rating_Title">Rating</p>
-     <input type="range" value="0" min="0" max="5" step="0.1" class="c-Modal-Have_Rating_Slider" oninput="document.getElementById('output1').value=this.value">
+     <input type="range" v-model="rating" min="0" max="5" step="0.1" class="c-Modal-Have_Rating_Slider" oninput="document.getElementById('output1').value=this.value">
      <output id="output1" class="c-Modal-Have_Rating_Num">0</output>
     </div><!-- /.c-Modal-Have_RatingSlider -->
 
-    <textarea name="" id="Input_Gear_Review" cols="30" rows="25" class="c-Modal-Have_Textarea" placeholder="レビューを記入してください。無記入でも登録できます。"></textarea>
+    <textarea name="comment" v-model="comment" id="Input_Gear_Review" cols="30" rows="25" class="c-Modal-Have_Textarea" placeholder="レビューを記入してください。無記入でも登録できます。"></textarea>
 
    </div><!-- /.c-Modal-Have_Content -->
 
    <footer class="c-Modal-Have_Footer">
     <button type="submit" class="c-Modal-Have_Register c-Form_Submit">登録</button>
    </footer>
-  </div><!--//c-Modal-Have-->
   </form>
+  </div><!--//c-Modal-Have-->
 
  </div><!--//c-Modal-Have-->
 </template>
@@ -39,8 +39,48 @@
   export default {
     name: 'HaveModal',
     computed: {
-        ...mapState('MultiModal', ['gear'])
+        //...mapState('MultiModal', ['gear']),
+        gear: {
+            get: function () { return this.$store.getters.gear },
+        },
+        have_count: {
+            get: function () { return this.$store.getters.have_count },
+        },
+        isHave: {
+            get: function () { return this.$store.getters.isHave },
+        },
+        comment: {
+            get: function () { return this.$store.getters.comment },
+        },
+        rating: {
+            get: function () { return this.$store.getters.rating },
+        },
+        isProcessing: {
+            get: function () { return this.$store.getters.isProcessing },
+        },
     },
     mixins: [MultiModalMixin],
+    methods: {
+        toggleHave() {
+            this.$store.commit("setLoading", false)
+            return new Promise((resolve, reject) => {
+                this.$store.commit("setProcessing", true)
+                axios.post(config.toggleWant, {
+                    gear_id: this.gear.id,
+                    count: this.have_count,
+                    type: this.isHave,
+                }).then(res => {
+                    console.log(res.data)
+                    this.have_count = res.data.count
+                    this.isHave = res.data.type
+                    this.$store.commit("setProcessing", false)
+                    resolve();
+                }).catch(error => {
+                    console.log(error)
+                    router.push({'path': '/login'});
+                });
+            });
+        }
+    }
   }
 </script>
--- resources/js/store/modules/MultiModal.js
+++ resources/js/store/modules/MultiModal.js
@@ -1,15 +1,43 @@
 const state = {
   modalName: "",
   gear: null,
+  have_count: 0,
+  isHave: 0,
+  comment: null,
+  rating: 0,
+  isProcessing: false
+}
+
+const getters = {
+    gear: state => state.gear,
+    have_count: state => state.have_count,
+    isHave: state => state.isHave,
+    comment: state => state.comment,
+    rating: state => state.rating,
+    isProcessing: state => state.isProcessing,
 }
 
 const mutations = {
   setModal (state, payload) {
     state.modalName = payload.name
     state.gear = payload.gear.gear 
+    state.have_count = payload.gear.gear.profile.have_count
+    state.isHave = payload.gear.gear.have_user_count
+    if (payload.gear.gear.have_users.have_comment != undefined) {
+        state.comment = payload.gear.gear.have_users.have_comment
+    }
+    if (payload.gear.gear.thander_users.point != undefined) {
+        state.comment = payload.gear.gear.thander_users.point
+    }
   },
   hideModal (state) {
     state.modalName = ''
+  },
+  setProcessing (state, payload) {
+    state.isProcessing = payload
+  },
+  setResponseHaveData (state, payload) {
+
   }
 }
 
