4b731990833b60d09b27fe878ebffbc502cecf57
2Q <fresh_2q@yahoo.co.jp>
2019-03-28 19:05:20 +0900

    web font追加

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,16 @@
   <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
   <meta name="format-detection" content="telephone=no">
   <link rel="stylesheet" href="https://use.typekit.net/vtq3xtc.css">
+  <script>
+(function(d) {
+ var config = {
+  kitId: 'wwa5tjo',
+  scriptTimeout: 3000,
+  async: true
+ },
+     h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+})(document);
+  </script>
   <link href="{{ mix('/css/app.css') }}?ver={{ time() }}" rel="stylesheet">
   <script type="text/javascript">
     window.Laravel = window.Laravel || {};
