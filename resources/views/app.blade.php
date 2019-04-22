f75ee3056e954122cbbc5a9ffc64a9c560544f63
2Q <fresh_2q@yahoo.co.jp>
2019-04-23 05:48:43 +0900

    head情報整理

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,10 +3,36 @@
  <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=Edge, chrome=1">
-  <title>Campbug</title>
+  <title>アウトドアギアを愛でる。Campbug｜{$BrandName} {$GearName}</title>
   <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
   <meta name="format-detection" content="telephone=no">
-  <link rel="stylesheet" href="https://use.typekit.net/vtq3xtc.css">
+
+  <meta property="og:url" content="{ページのURL}" />
+  <meta property="og:title" content="{ページのタイトル}" />
+  <meta property="og:type" content="website">
+  <meta property="og:description" content="{記事の抜粋}" />
+  <meta property="og:image" content="(画像のURL}" />
+
+  <meta name="twitter:card" content="summarylargeimage" />
+  <meta name="twitter:site" content="@Campbug_Outdoor" />
+
+  <meta property="og:site_name" content="Campbug" />
+  <meta property="og:locale" content="ja_JP" />
+  <meta property="fb:app_id" content="658430184592123" />
+
+
+  <meta name="msapplication-square150x150logo" content="{{ asset('img/favicon/site-tile-150x150.png') }}">
+  <meta name="msapplication-square310x310logo" content="{{ asset('img/favicon/site-tile-310x310.png') }}">
+  <meta name="msapplication-TileImage" content="画像のURL" />
+  <meta name="msapplication-TileColor" content="#FCFBF9">
+  <meta name="theme-color" content="#FCFBF9">
+  <link rel="icon" type="image/x-icon" href="{{ asset('img/favicon/favicon.ico') }}">
+  <link rel="apple-touch-icon-precomposed" sizes="180x180" href="{{ asset('img/favicon/apple-touch-icon-180x180.png') }}" />
+  <link rel="mask-icon" href="{{ asset('img/favicon/safari-icon.svg') }}" color="#FCFBF9" />
+  <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('img/favicon/android-chrome-192x192.png') }}">
+  <link rel="manifest" href="{{ asset('img/favicon/manifest.json') }}">
+
+
   <script>
 (function(d) {
  var config = {
@@ -17,6 +43,7 @@
      h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
 })(document);
   </script>
+  <link rel="stylesheet" href="https://use.typekit.net/vtq3xtc.css">
   <link href="{{ mix('/css/app.css') }}?ver={{ time() }}" rel="stylesheet">
   <script type="text/javascript">
     window.Laravel = window.Laravel || {};
