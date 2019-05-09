c728d97c22a4cd91e45abbddfaa465e3987ebbd2
2Q <fresh_2q@yahoo.co.jp>
2019-05-10 02:37:33 +0900

    title 修正

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
  <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=Edge, chrome=1">
-  <title>アウトドアギアを愛でる。Campbug｜{$BrandName} {$GearName}</title>
+  <title>アウトドアギア大百科 Campbug｜{$BrandName} {$GearName}</title>
   <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
   <meta name="format-detection" content="telephone=no">
 
