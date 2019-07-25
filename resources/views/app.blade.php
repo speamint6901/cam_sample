6d1ea3cb9a53f63ae93b3267dcbc8a3834ec4da8
2Q <fresh_2q@yahoo.co.jp>
2019-07-25 22:58:09 +0900

    googleタグ追加

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -65,10 +65,15 @@
 
  <body>
 
-  <!-- Google Tag Manager (noscript) -->
-  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KL362DC"
-                    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-  <!-- End Google Tag Manager (noscript) -->
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-139547908-1"></script>
+  <script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-139547908-1');
+  </script>
 
 
   <div id="app">
