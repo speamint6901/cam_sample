519a585aa69e699a4b9a58362ee07df805475080
j-nasu <speamint6901@gmail.com>
2019-11-27 10:38:36 +0900

    アフィリエイトに画像追加

--- resources/js/components/pages/Gear.vue
+++ resources/js/components/pages/Gear.vue
@@ -111,9 +111,9 @@
      </ul><!-- /.c-MasterItem_SNSshare -->
 
      <div class="c-MasterItem_ProductLink">
-      <a v-if="gear_detail.amazon_url" :href="gear_detail.amazon_url" target="_blank" rel="nofollow" class="c-MasterItem_ProductLink_Btn -Amazon" onClick="ga('send', 'event', 'affiliate', 'click', 'Amazon_Link');">Amazon</a>
-      <a v-if="gear_detail.rakuten_url" :href="gear_detail.rakuten_url" target="_blank" rel="nofollow" class="c-MasterItem_ProductLink_Btn -Rakuten" onClick="ga('send', 'event', 'affiliate', 'click', 'Rakuten_Link');">楽天市場</a>
-      <a v-if="gear_detail.yahoo_url" :href="gear_detail.yahoo_url" target="_blank" rel="nofollow" class="c-MasterItem_ProductLink_Btn -Yahoo" onClick="ga('send', 'event', 'affiliate', 'click', 'Yahoo_Link');">Yahooショッピング</a>
+      <a v-if="gear_detail.amazon_url" :href="gear_detail.amazon_url" target="_blank" rel="nofollow" class="c-MasterItem_ProductLink_Btn -Amazon" onClick="ga('send', 'event', 'affiliate', 'click', 'Amazon_Link');">{{ gear_detail.amazon_img }}Amazon</a>
+      <a v-if="gear_detail.rakuten_url" :href="gear_detail.rakuten_url" target="_blank" rel="nofollow" class="c-MasterItem_ProductLink_Btn -Rakuten" onClick="ga('send', 'event', 'affiliate', 'click', 'Rakuten_Link');">{{ gear_detail.rakuten_img }}楽天市場</a>
+      <a v-if="gear_detail.yahoo_url" :href="gear_detail.yahoo_url" target="_blank" rel="nofollow" class="c-MasterItem_ProductLink_Btn -Yahoo" onClick="ga('send', 'event', 'affiliate', 'click', 'Yahoo_Link');">{{ gear_detail.yahoo_img }}Yahooショッピング</a>
      </div><!-- /.c-MasterItem_ProductLink -->
 
     </div><!-- /.c-MasterItem_MainOverview -->
